dbb93cf3a481a9b2ea7101d8de1dd7e7c1fa1f87
Jasper Niemeyer <jani5714@colorado.edu>
2023-11-15 16:39:37 -0700

    added another alt

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -90,6 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
+                        alt="idk why vercel didn't include one in their demo"
                       />
                       <p>{invoice.name}</p>
                     </div>
