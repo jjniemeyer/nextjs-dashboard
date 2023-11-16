446d405564169a94b13420538b9fbf055a77138c
Jasper Niemeyer <jani5714@colorado.edu>
2023-11-16 12:25:56 -0700

    dynamic rendering on dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,12 @@ import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data
 export default async function Page() {
   const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
-  const {numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices} = await fetchCardData();
+  const {
+    numberOfCustomers,
+    numberOfInvoices,
+    totalPaidInvoices,
+    totalPendingInvoices,
+    } = await fetchCardData();
 
   return (
     <main>
